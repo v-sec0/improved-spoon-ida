38e47aa88039f13d86a88e67c67b36b87b60cd3a
Levon <levon@ihatemalware.com>
2024-08-29 20:00:26 -0500

    updated code to echo entries to server

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-console.log("App started.")
-app.set('view engine', 'ejs')
+// Allows express to parse JSON objects.
+app.use(express.json());
 
-app.use(express.static('./public/'))
-app.get('/', function (req, res) {
-  res.sendFile('./public/index.html')
-})
+// Confirmation that the app is working server-side.
+console.log("App started.");
 
-app.get('/ejs', function (req, res) {
-  res.render('index', {
-    myServerVariable : "something"
-  });
+// Serves a static site from the public directory
+app.use(express.static("public"));
+app.get("/", function (req, res) {
+  res.sendFile("/public/index.html");
+});
 
-})
+app.post("/", function (req, res) {
+  const { message } = req.body;
+  console.log(message);
+  res.status(201).send({ messageRecieved: message });
+  res.end();
+});
 
-app.listen(3000)
+app.listen(3000);
+
+// Process for developing this functionality
+// 1. Looked up "parsing JSON from response with express"
