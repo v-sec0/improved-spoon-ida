93c9927c0ba5cac25f37ee38619d2029f0608fb0
Watkins <lwatkins6@una.edu>
2024-09-03 13:45:39 -0500

    messing with ejs and mongo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+
 console.log("App started.")
 app.set('view engine', 'ejs')
 
@@ -15,5 +16,34 @@ app.get('/ejs', function (req, res) {
   });
 
 })
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.uri;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(3000)
