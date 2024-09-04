f2a428ad13755080d2da3861abe29ac067b2cffd
Levon <levon@ihatemalware.com>
2024-09-04 15:28:58 -0500

    removed html, enhanced ejs, changed site to message board

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 const express = require("express");
 const app = express();
 
+// Setting default view engine
+app.set("view engine", "ejs");
+
 // Allows express to parse JSON objects.
 app.use(express.json());
-
-// Confirmation that the app is working server-side.
-console.log("App started.");
 
 // Serves a static site from the public directory
 app.use(express.static("public"));
 app.get("/", function (req, res) {
-  res.status(201).render("views/index");
+  res.render("index", { username: "{empty username}", post: "{empty post}" });
+  res.end();
 });
 
 // Taking request from client and echoing to console and sending it back to be displayed on website.
 app.post("/", function (req, res) {
-  const { message } = req.body;
-  console.log(message);
-  res.status(201).send({ messageRecieved: message });
+  const { username, post } = req.body;
+
+  // Get copy of entry onto server console
+  console.log("username = %s : post = %s", username, post);
+
+  res.send({ post: post, username: username });
   res.end();
 });
 
-app.listen(3000);
-
+app.listen(3000, () => {
+  console.log("App started listening port 3000");
+});
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,13 +1,15 @@
 $(document).ready(function () {
-  const submitButton = $("#sendBtn");
-  const clientMessageBox = $("#serverMessageInput");
-  const serverTerminal = $("#serverResponse");
+  const submitButton = $("#subButton");
+  const clientPost = $("#postInput");
+  const clientUser = $("#usernameInput");
+  const postEntry = $("#postEntry");
+  const userEntry = $("#userEntry");
 
   submitButton.on("click", () => {
     console.log("Button clicked!");
 
     // Turning input into variable
-    let clientMessage = { message: clientMessageBox.val() };
+    let requestBody = { post: clientPost.val(), username: clientUser.val() };
 
     // Making POST request options
     const responseOptions = {
@@ -15,14 +17,15 @@ $(document).ready(function () {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(clientMessage), // Need this because the message is JSON
+      body: JSON.stringify(requestBody), // Need this because the message is JSON
     };
 
     // Getting response from server and using that to replace terminal message.
     fetch("/", responseOptions)
       .then((response) => response.json())
       .then((data) => {
-        serverTerminal.html(data.messageRecieved);
+        postEntry.html(data.post);
+        userEntry.html(data.username);
       });
   });
 });
