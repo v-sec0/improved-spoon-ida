88ed0c590ae237b471a07dd5f19e43bb54e8b180
Watkins <lwatkins6@una.edu>
2024-09-05 12:48:24 -0500

    updated wording, added mongo and env

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
 
@@ -25,6 +26,35 @@ app.post("/", function (req, res) {
   res.end();
 });
 
-app.listen(3000, () => {
-  console.log("App started listening port 3000");
+// ------------------------------ MONGO STUFF -------------------------------------
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.URI;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(process.env.PORT, () => {
+  console.log("App started listening port %d", process.env.PORT);
 });
