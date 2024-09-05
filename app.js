6f24c9ba179a5b07c2c12bdf1962c1c3837f7288
Levon <levon@ihatemalware.com>
2024-09-05 14:47:28 -0500

    fixed mongoDB and connect postboard to db

--- app.js
+++ app.js
@@ -8,24 +8,6 @@ app.set("view engine", "ejs");
 // Allows express to parse JSON objects.
 app.use(express.json());
 
-// Serves a static site from the public directory
-app.use(express.static("public"));
-app.get("/", function (req, res) {
-  res.render("index", { username: "{empty username}", post: "{empty post}" });
-  res.end();
-});
-
-// Taking request from client and echoing to console and sending it back to be displayed on website.
-// app.post("/", function (req, res) {
-//   const { username, post } = req.body;
-
-//   // Get copy of entry onto server console
-//   console.log("username = %s : post = %s", username, post);
-
-//   res.send({ post: post, username: username });
-//   res.end();
-// });
-
 // ------------------------------ MONGO STUFF -------------------------------------
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -56,10 +38,12 @@ async function run() {
 run().catch(console.dir);
 
 // Endpoint for database retrieval
+app.use(express.static("public"));
 app.get("/mongo", async (req, res) => {
   await client.connect();
   const results = await client.db("postboard").collection("posts").find({}).toArray();
   res.render("index", {mongoResults: results})
+  console.log(results)
 });
 
 app.listen(process.env.PORT, () => {
