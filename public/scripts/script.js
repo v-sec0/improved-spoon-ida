bbb0e87218e8cc28f828557c2fb11bc5bcdcc474
Levon Watkins Jr. <levon@ihatemalware.com>
2024-10-20 15:00:56 -0500

    updated code and added some styles

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,24 +1,27 @@
 jQuery(() => {
-  console.log("Ready to rumble!");
-  (() => {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = $('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-  
-        
+    console.log("Ready to rumble!");
+    (() => {
+        'use strict'
 
-        form.classList.add('was-validated')
-        $("#submitButton").html("Submitted!")
-      }, false)
-    })
-  })()
+        // Variables
+        let submitButton = $("#submitButton");
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = $('.needs-validation')
+
+        // Loop over them and prevent submission
+        Array.from(forms).forEach(form => {
+            form.addEventListener('submit', event => {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                } else {
+                    submitButton.html("check")
+                    submitButton.removeClass("submit")
+                    submitButton.addClass("edit");
+                }
+                form.classList.add('was-validated')
+            }, false)
+        })
+    })()
 });
--- public/scripts/update.js
+++ public/scripts/update.js
@@ -1,22 +1,49 @@
-function updatePost(postId) { 
+function updatePost(postId, mode) {
 
-    // Making content editable
-    $(`.${postId}`).attr('contenteditable', 'true'); 
+    // Defining variables
+    let postcardHeader = $(`#postheader-${postId}`);
+    let postcard = $(`#postcard-${postId}`);
+    let updClickable = $(`#updClickable`)
+    let delClickable = $(`#delClickable`)
+    let post = $(`.${postId}`)
 
-    // Chaning to submit button
-    $(`#upd-${postId}`).html("Update");
-    $(`#upd-${postId}`).addClass("btn-success");
-    $(`#upd-${postId}`).removeClass("btn-warning");
-    $(`#upd-${postId}`).attr("onclick", `submitNewPost("${postId}")`); 
+    // Evaluating the mode argument
+    if (mode === 'edit') {
 
-    // Changing the delete button
-    $(`#del-${postId}`).html("Cancel");
-    $(`#del-${postId}`).attr("href", "/"); 
+        // Making content editable
+        post.attr('contenteditable', 'true');
 
-    // Changing the postcard color
-    $(`#postcard-${postId}`).addClass("border-warning");
-    $(`#postcard-${postId}`).addClass("text-bg-dark");
-    $(`#postheader-${postId}`).html("⚠️ EDIT MODE ⚠️")
-};
+        // Changing to submit button
+        updClickable.html("check");
+        updClickable.attr("onclick", `submitNewPost("${postId}")`);
+
+        // Changing the delete button
+        delClickable.html("cancel");
+        delClickable.attr("onclick", `updatePost("${postId}", "cancel")`);
+
+        // Changing the postcard color
+        postcard.addClass("border-warning");
+        postcard.addClass("text-bg-dark");
+        postcardHeader.html("⚠️ EDIT MODE")
+    } else if (mode === 'cancel') {
+
+        console.log("Button clicked")
+
+        // Reverting changes
+        post.attr('contenteditable', 'false');
+
+        updClickable.html("edit_note");
+        updClickable.attr("onclick", `updatePost("${postId}", "edit")`);
+
+        delClickable.attr("onclick", `deletePost("${postId}"`);
+        delClickable.html("delete");
+
+        postcard.removeClass("border-warning");
+        postcard.removeClass("text-bg-dark");
+        postcardHeader.html(`Post ID: ${postId}`);
+    }
+
+
+}
 
 
